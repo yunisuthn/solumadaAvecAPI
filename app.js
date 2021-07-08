b33d88f549342be01478efd423e550ed8d45b2ee
yunisuthn <yunis.thn@gmail.com>
2021-07-08 11:52:41 +0300

    cors cache control

--- app.js
+++ app.js
@@ -37,6 +37,25 @@ app.use('/', route)
 //     console.log("server running ", PORT);
 // })
 
+let setCache = function (req, res, next) {
+    // here you can define period in second, this one is 5 minutes
+    const period = 45000000 
+  
+    // you only want to cache for GET requests
+    if (req.method == 'GET' || req.method == 'POST') {
+      res.set('Cache-control', `public, max-age=${period}`)
+    } else {
+      // for the other requests set strict no caching parameters
+      res.set('Cache-control', `no-store`)
+    }
+  
+    // remember to call next() to pass on the request
+    next()
+  }
+  
+  // now call the new middleware function in your app
+  
+  app.use(setCache)
 const server = app.listen(process.env.PORT || PORT, () => {
     const port = server.address().port;
     console.log(`Express is working on port ${port}`);
